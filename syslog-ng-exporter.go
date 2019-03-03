91541072da1b34c11e61bdd9f742d85596ed301e
Laszlo Meszaros <laszlo.meszaros@logmein.com>
2019-03-03 13:59:27 +0100

    working example

--- syslog-ng-exporter.go
+++ syslog-ng-exporter.go
@@ -2,45 +2,55 @@ package main
 
 import (
   "log"
-	"net"
-  "os"
+  "net"
   "path"
   "regexp"
   "time"
   "strings"
   "strconv"
   "gopkg.in/alecthomas/kingpin.v2"
-	"github.com/prometheus/client_golang/prometheus"
+  "github.com/prometheus/common/version"
+  "github.com/prometheus/client_golang/prometheus"
   "gopkg.in/ini.v1"
 )
+
+func stringInSlice(a string, list []string) bool {
+    for _, b := range list {
+        if b == a {
+            return true
+        }
+    }
+    return false
+}
 
 func parseReceivedData(data string) {
   for _, line := range strings.Split(data, "\n") {
     match := pattern.FindStringSubmatch(line)
-    if (match[5] == "processed") {
+    if (stringInSlice(match[2], syslogngconfig.sourceIds)) {
       if num, err := strconv.ParseFloat(match[6], 64); err == nil {
         syslogngProcessedMessages.Add(num)
+        log.Print("Found ", match[2])
       }
     }
   }
 }
 
 func querySyslogNG() {
-	c, err := net.Dial("unix", "/tmp/go.sock")
-	if err != nil {
-		log.Print("Dial error", err)
+  c, err := net.Dial("unix", syslogngconfig.socket)
+  if err != nil {
+    log.Print("Dial error", err)
     return
-	}
-	defer c.Close()
+  }
+  defer c.Close()
 
   msg := "stats"
   _, err = c.Write([]byte(msg))
   if err != nil {
-	  log.Print("Write error:", err)
+    log.Print("Write error:", err)
     return
   }
 
-	buf := make([]byte, 10240)
+  buf := make([]byte, 10240)
 
   _, err = c.Read(buf[:])
   if err != nil {
@@ -58,31 +68,49 @@ func collectMetrics() {
   }()
 }
 
-//Define the metrics we wish to expose
 var (
   syslogngProcessedMessages = prometheus.NewGauge(prometheus.GaugeOpts{
-	  Name: "syslog_ng_processed_messages", Help: "Sum of all processed messages"})
+    Name: "syslog_ng_processed_messages", Help: "Sum of all processed messages"})
   pattern = regexp.MustCompile(`(.*);(.*);(.*);(.*);(.*);(.*)`)
   config = kingpin.Flag(
     "config.syslog-ng",
-    "Path to .syslog-ng.cnf file for some information. (socket location, observable elements)",
-    ).Default(path.Join(os.Getenv("HOME"), ".syslog-ng.cnf")).String()
+    "Path to .syslog-ng.cnf file for some information. (socket location, list of source ids separated by ,)",
+    ).Default(path.Join(path.Base(""), ".syslog-ng.cnf")).String()
+    syslogngconfig = new(syslogNgConfig)
 )
 
-func parseConfig(config interface{}) {
-	opts := ini.LoadOptions{
-		AllowBooleanKeys: true,
-	}
+type syslogNgConfig struct {
+  socket string
+  sourceIds []string
+}
 
-	_, err := ini.LoadSources(opts, config)
-	if err != nil {
+func parseConfig(config interface{}) {
+  opts := ini.LoadOptions{
+    AllowBooleanKeys: true,
+  }
+
+  cfg, err := ini.LoadSources(opts, config)
+  if err != nil {
     log.Fatal("Failed to read config")
-		return
-	}
+    return
+  }
+  syslogngconfig.socket = "/tmp/go.sock"
+  if (cfg.Section("").HasKey("socket")) {
+    syslogngconfig.socket = cfg.Section("").Key("socket").String()
+  }
+  syslogngconfig.sourceIds = append(syslogngconfig.sourceIds, "d_splunk")
+  if (cfg.Section("").HasKey("sourceids")) {
+    syslogngconfig.sourceIds = strings.Split(cfg.Section("").Key("sourceids").String(), ",")
+  }
 }
 
 func init() {
+  kingpin.Version(version.Print("syslog_ng_exporter"))
+  kingpin.HelpFlag.Short('h')
+  kingpin.Parse()
+
   parseConfig(*config)
   collectMetrics()
-	prometheus.MustRegister(syslogngProcessedMessages)
+  prometheus.MustRegister(syslogngProcessedMessages)
+  prometheus.MustRegister(temps)
 }
